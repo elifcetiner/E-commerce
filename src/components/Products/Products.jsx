9a681a86bf66bc3249905ef90eee625f6f151b35
Elif Çetiner <elifcetineer@gmail.com>
2021-08-06 01:06:09 +0300

    initial commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import {Grid} from '@material-ui/core';
+
 import Product from './Product/Product';
-import useStyles from './styles';
+import useStyles from './Product/styles';
 
 const products = [
-    {id: '1', name: 'Shoes', description: 'Running shoes.', price:'₺5'},
-    {id: '2', name: 'Macbook', description: 'Apple Macbook.', price:'₺10'},
+    {id: '1', name: 'Shoes', description: 'Running shoes.', price:'₺5', image:' '},
+    {id: '2', name: 'Macbook', description: 'Apple Macbook.', price:'₺10',image:' '},
 ];
 
 const Products = () => {
+    return(
     <main>
     <Grid container justify="center" spacing={4}> 
     {
@@ -16,10 +18,9 @@ const Products = () => {
             <Grid item key={product.id} xs={12}  sm={6}  md={4}  lg={3}>
                 <Product product = {product}/>
             </Grid>
-        ))
-        
-        }
+        ))}
     </Grid>
     </main>
+    );
 }
 export default Products;
